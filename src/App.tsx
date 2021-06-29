09a82cb3296e46227f3769f4c26f7bb300cdfeb8
carwbrown <carwbrown@google.com>
2021-06-28 21:01:54 -0400

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+/*
+enum WANTS {
+  wave_clear = "wave clear",
+}
+
+Camp Clear
+Burst Damage
+Sustained Damage
+Healer
+Tank
+Engage
+Followup
+(Physical or Spell)
+*/
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>HotS Drafter</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
